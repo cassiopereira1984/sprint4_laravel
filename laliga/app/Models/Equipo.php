0c07fbe44f33b24165c5c1c5c2a285d71471f74a
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-05-22 11:30:35 +0200

    model equipo

--- laliga/app/Models/Equipo.php
+++ laliga/app/Models/Equipo.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Equipo extends Model
 {
-    use HasFactory;
+    protected $table = 'equipos';
+
+    // Los campos que pueden ser asignados masivamente
+    protected $fillable = ['nombre', 'ciudad', 'logo', 'fundacion', 'estadio'];
 }
+?>
