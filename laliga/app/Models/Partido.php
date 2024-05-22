c74f9df451c85e6b4d78fa9539e1d2fb568e7a30
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-05-22 11:35:50 +0200

    code partido

--- laliga/app/Models/Partido.php
+++ laliga/app/Models/Partido.php
@@ -1,11 +1,29 @@
 <?php
-
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Partido extends Model
 {
-    use HasFactory;
+    protected $table = 'equipos';
+
+    protected $fillable = [
+        'equipo_local_id',
+        'equipo_visitante_id',
+        'fecha',
+        'resultado_local',
+        'resultado_visitante',
+        'estadio',
+    ];
+
+    public function equipo_local_id()
+    {
+        return $this->belongsTo(Equipo::class, 'equipo_local_id');
+    }
+
+    public function equipo_visitante_id()
+    {
+        return $this->belongsTo(Equipo::class, 'equipo_visitante_id');
+    }
 }
+?>
